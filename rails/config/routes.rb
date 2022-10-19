d19da35b77a50565dff74140ef3383a2e7bf0802
tegnike <o3nike.teg.14@gmail.com>
2022-10-19 22:00:36 +0200

    devise 導入

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'health_check', to: 'application#health_check'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope :api, format: 'json' do
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+      # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+      # Defines the root path route ("/")
+      # root "articles#index"
+    end
+  end
 end
